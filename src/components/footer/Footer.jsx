59e899885789930dc347db61e7bc2da6a2578477
Trương Hữu Thọ <96384986+Havold@users.noreply.github.com>
2024-10-12 02:44:04 +0700

    Create UI Navbar and Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import styles from "./Footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div>2024 METAN. All rights reserved.</div>
-  )
-}
+    <div className={styles.container}>
+      <div>2024 METAN. All rights reserved.</div>
+      <div className={styles.social}>
+        <Image className={styles.icon} src='/1.png' width={15} height={15} alt='METAN social' />
+        <Image className={styles.icon} src='/2.png' width={15} height={15} alt='METAN social' />
+        <Image className={styles.icon} src='/3.png' width={15} height={15} alt='METAN social' />
+        <Image className={styles.icon} src='/4.png' width={15} height={15} alt='METAN social' />
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,48 +1,55 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-    const links = [{
-        id: 1,
-        title: 'Home',
-        href: '/'
+  const links = [
+    {
+      id: 1,
+      title: "Home",
+      href: "/",
     },
     {
-        id: 2,
-        title: 'Portfolio',
-        href: '/portfolio'
+      id: 2,
+      title: "Portfolio",
+      href: "/portfolio",
     },
     {
-        id: 3,
-        title: 'Blog',
-        href: '/blog'
+      id: 3,
+      title: "Blog",
+      href: "/blog",
     },
     {
-        id: 4,
-        title: 'About',
-        href: '/about'
+      id: 4,
+      title: "About",
+      href: "/about",
     },
     {
-        id: 5,
-        title: 'Contact',
-        href: '/contact'
+      id: 5,
+      title: "Contact",
+      href: "/contact",
     },
     {
-        id: 6,
-        title: 'Dashboard',
-        href: '/dashboard'
+      id: 6,
+      title: "Dashboard",
+      href: "/dashboard",
     },
-]
+  ];
   return (
-    <div>
-        <Link href='/'>METAN</Link>
-        <div>
-            {links.map((link) => {
-                return (<Link key={link.id} href={link.href}>{link.title}</Link>)
-            })}
-        </div>
+    <div className={styles.container}>
+      <Link className={styles.logo} href="/">METAN</Link>
+      <div className={styles.links}>
+        {links.map((link) => {
+          return (
+            <Link className={styles.link} key={link.id} href={link.href}>
+              {link.title}
+            </Link>
+          );
+        })}
+        <button className={styles.logoutBtn}>Logout</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
